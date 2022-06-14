60d35fdfbbc4603c347aa7bc9bc9126a0b2554cf
“Hardteiron” <fengte6868@163.com>
2022-06-14 08:52:42 +0800

    Initial commit

---
+++ src/main/java/com/liuyanzhao/ssm/blog/dto/UploadFileVO.java
@@ -0,0 +1,16 @@
+package com.liuyanzhao.ssm.blog.dto;
+
+import lombok.Data;
+
+/**
+ * @author fengte
+ */
+
+@Data
+public class UploadFileVO {
+
+    private String src;
+
+    private String title;
+
+}
